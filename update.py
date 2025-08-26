035a654e443f18b5872c21c607406b323cf72770
Tharindu Prabath <116081100+tharindu899@users.noreply.github.com>
2025-08-26 23:43:27 +0530

    Update update.py

--- update.py
+++ update.py
@@ -32,7 +32,7 @@ def update():
     AUPR = config("ALWAYS_UPDATE_PY_REQ", default=False, cast=bool)
     UPSTREAM_REPO = config(
         "UPSTREAM_REPO",
-        default="https://github.com/Nubuki-all/Enc")
+        default="https://github.com/tharindu899/Enc")
     UPSTREAM_BRANCH = config("UPSTREAM_BRANCH", default="main")
 
     r_filep = Path("filters/Auto-rename.txt")
